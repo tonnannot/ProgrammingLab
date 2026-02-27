a9f96a3e3e6561045092646e9eeefb5a738b0b11
Tonna <chasinos2.0@gmail.com>
2026-02-27 11:18:29 +0100

    Aggiunti esercizi 1 e 2 in Python

---
+++ es1.py
@@ -0,0 +1,6 @@
+minuti = 538
+
+ore = minuti // 60
+resto = minuti % 60
+
+print(f"{ore}h:{resto:02d}min")
---
+++ es2.py
@@ -0,0 +1,7 @@
+n = int(input("Inserisci un numero intero: "))
+
+quadrato = n ** 2
+cubo = n ** 3
+
+print("Quadrato:", quadrato)
+print("Cubo:", cubo)
